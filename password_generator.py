d8bc0a3744bfdb66e1e9f7b91055f32d9992a201
Petar Todorov <154418284+PetarTdrv@users.noreply.github.com>
2024-01-03 21:20:24 +0200

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,6 +1,8 @@
 # More or less, but maybe more
 import random
- 
+
+lenght = int(input("Enter desired password length (4-8): "))
+
 symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '?', '_']
 random_symbols = random.choice(symbols)
  
@@ -12,6 +14,18 @@ random_lower_letters = random.choice(lower_letters)
  
 numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9',]
 random_numbers = random.choice(numbers)
- 
-password = f"{random_upper_letters}{random_lower_letters}{random_numbers}{random_symbols}"
+
+password = "_"
+
+if lenght == 4:
+    password = f"{random_upper_letters}{random_symbols}{random_numbers}{random_lower_letters}"
+elif lenght == 5:
+    password = f"{random_upper_letters}{random_symbols}{random_numbers}{random_lower_letters}{random_upper_letters}"
+elif lenght == 6:
+    password = f"{random_lower_letters}{random_symbols}{random_numbers}{random_upper_letters}{random_lower_letters}{random_numbers}"
+elif lenght == 7:
+    password = f"{random_upper_letters}{random_symbols}{random_numbers}{random_lower_letters}{random_upper_letters}{random_numbers}{random_lower_letters}"
+elif lenght == 8:
+    password = f"{random_upper_letters}{random_symbols}{random_numbers}{random_lower_letters}{random_upper_letters}{random_lower_letters}{random_numbers}{random_upper_letters}" 
+
 print(password)
