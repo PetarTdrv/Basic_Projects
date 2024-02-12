1d9ec90b44cf2a88e9ac81dc43188010f68ca466
Petar Todorov <154418284+PetarTdrv@users.noreply.github.com>
2024-02-12 21:03:51 +0200

    Delete Years_months_days.py

--- Years_months_days.py
+++
@@ -1,6 +0,0 @@
-years = int(input("Enter years: "))
-months = years * 12
-days_in_year = 365.242199
-days = days_in_year * years
-
-print(f"Years - {years} -> Months - {months} -> Days - {days:.0f}")
